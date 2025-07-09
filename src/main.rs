c1d2bbcf995cad12d5580d629e7895a04b45016e
Adrian <enevadrian@gmail.com>
2025-07-09 12:01:36 +0300

    Removed manual ip input, implemented retreival of location using seperate structs for city/country/location

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-mod utils;
 use maxminddb::Reader;
 use serde::Deserialize;
-use utils::read_line;
 
 #[derive(Debug, Deserialize)]
 struct GeoInfo {
-    city: Option<Names>,
-    country: Option<Names>,
+    city: Option<City>,
+    country: Option<Country>,
     location: Option<Location>,
 }
 
 #[derive(Debug, Deserialize)]
-struct Names {
-    names: std::collections::HashMap<String, String>,
+struct City {
+    names: Option<std::collections::HashMap<String, String>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Country {
+    names: Option<std::collections::HashMap<String, String>>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -21,20 +24,39 @@ struct Location {
     longitude: Option<f64>,
 }
 
+
 fn main() {
     // Path to the mounted .mmdb file (adjust if needed)
     let reader = Reader::open_readfile("data/GeoLite2-City.mmdb")
         .expect("Failed to open GeoLite2-City.mmdb");
 
-    // Add ip input manually
-    println!("Enter example ip");
-    let ip = read_line();
+    let ip = "8.8.8.8";
 
     // Parse IP address and lookup
     let ip_addr = ip.parse::<std::net::IpAddr>()
         .expect("Invalid IP address format");
-    let geo = reader.lookup::<GeoInfo>(ip_addr)
-        .expect("Failed to lookup IP");
+    let geo: GeoInfo = reader.lookup(ip_addr).expect("Failed to lookup IP")
+        .expect("Failed to parse GeoInfo");
+
+    // Get city and country
+    let city = geo.city
+        .as_ref()
+        .and_then(|c| c.names.as_ref())
+        .and_then(|names| names.get("en"))
+        .map_or("Unknown City".to_string(), |s| s.clone());
     
-    println!("{:?}", geo);
+    let country = geo.country
+        .as_ref()
+        .and_then(|c| c.names.as_ref())
+        .and_then(|names| names.get("en"))
+        .map_or("Unknown Country".to_string(), |s| s.clone());
+
+    let location = geo.location
+        .as_ref()
+        .map(|l| format!("{}, {}", l.latitude.unwrap_or(0.0), l.longitude.unwrap_or(0.0)))
+        .unwrap_or_else(|| "Unknown Location".to_string());
+    
+    println!("City: {}", city);
+    println!("Country: {}", country);
+    println!("Location: {}", location);
 }
